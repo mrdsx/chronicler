a36282de60401ce3643dbfd0dd5c603ed60963ec
mrdsx <andre0.castlegames@gmail.com>
2025-07-01 14:08:24 +0300

    feat(backend/auth): change return type in users_db.py from orm to dict

--- server/app/db/users_db.py
+++ server/app/db/users_db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from models.user import UserSchema
 
 engine = create_engine('sqlite:///app/db/users.db')
 
@@ -26,8 +27,16 @@ def save_user(user: DB_User_Model):
     session.add(new_user)
     session.commit()
 
-def get_user_by_username(username: str):
-    return session.query(DB_User_Model).filter(DB_User_Model.username == username).first()
+def get_user_by_username(username: str) -> UserSchema | None:
+    try:
+        db_user = session.query(DB_User_Model).filter(DB_User_Model.username == username).first()
+        return UserSchema.model_validate(db_user).model_dump()
+    except BaseException:
+        return None
 
-def get_user_by_email(email: str):
-    return session.query(DB_User_Model).filter(DB_User_Model.email == email).first()
+def get_user_by_email(email: str) -> UserSchema | None:
+    try:
+        db_user = session.query(DB_User_Model).filter(DB_User_Model.email == email).first()
+        return UserSchema.model_validate(db_user).model_dump()
+    except BaseException:
+        return None
