216a001446bc2c9a24fd1258468ab97a5ab4c937
mrdsx <andre0.castlegames@gmail.com>
2025-07-01 14:30:07 +0300

    nothing happened

--- server/app/routes/auth_routes.py
+++ server/app/routes/auth_routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
+from utils.validation_utils import get_is_login_data_valid
+from db.users_db import get_user_by_email
 from models.user import AuthModel_SignUp, AuthModel_Login
 from services.auth_services import create_user, validate_signup_data
 from constants import routes
-from routes.auth_routes import Auth
+from auth import Auth
 
 auth_handler = Auth()
 security = HTTPBearer()
@@ -26,26 +28,24 @@ async def signup(signup_data: AuthModel_SignUp):
 @router.post("/login")
 async def login(user_details : AuthModel_Login):
     try:
-        # TODO: implement a database to hold user email and hash password
-        # user = users_db.get(user_details.email)
-        # if user is None:
-        #       return HTTPException(status_code=401, detail='Wrong email or password')
-        hashed_password = auth_handler.encode_password(user_details.password)
-        if user_details.email is None:
-            return HTTPException(status_code=401, detail='Invalid email')
-        if not auth_handler.verify_password(user_details.password, hashed_password):
-            return HTTPException(status_code=401, detail='Invalid password')
-
+        db_user = get_user_by_email(user_details.email)
+        if db_user is None:
+            return HTTPException(status_code=401, detail="Wrong email or password")
+        
+        email = user_details.email
+        password = user_details.password
+        if not get_is_login_data_valid(email, password, db_user["hashed_password"]):
+            return HTTPException(status_code=401, detail="Wrong email or password")
+        
         access_token = auth_handler.encode_token(user_details.email)
         refresh_token = auth_handler.encode_refresh_token(user_details.email)
-        return {'access_token': access_token, 'refresh_token': refresh_token}
+        return {"access_token": access_token, "refresh_token": refresh_token}
     except BaseException as e:
-        print('Failed to do something: ' + str(e))
-        error_msg = 'Failed to login user'
-        return error_msg
+        print("Failed to do something: " + str(e))
+        error_msg = "Failed to login"
+        return HTTPException(status_code=400, detail=error_msg)
 
-@router.get('/secret')
+@router.get("/secret")
 async def secret_data(credentials: HTTPAuthorizationCredentials = Security(security)):
     token = credentials.credentials
     return auth_handler.decode_token(token)
-    
