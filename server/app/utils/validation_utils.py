f29933334aa4b7fd06e974bde6f28f9d311952bc
mrdsx <andre0.castlegames@gmail.com>
2025-07-01 14:19:37 +0300

    feat(backend/validation): add login data validation function

--- server/app/utils/validation_utils.py
+++ server/app/utils/validation_utils.py
@@ -1,9 +1,19 @@
 from email_validator import validate_email, EmailNotValidError
+from auth import Auth
 
-def get_is_email_valid(email: str):
+auth_handler = Auth()
+
+def get_is_email_valid(email: str) -> tuple[bool, str]:
     try:
         email_info = validate_email(email, check_deliverability=True)
         email = email_info.normalized
         return True, email
     except EmailNotValidError as e:
         return False, str(e)
+
+def get_is_login_data_valid(email: str, input_password: str, db_password: str):
+    if email is None:
+        return False
+    if not auth_handler.verify_password(input_password, db_password):
+        return False
+    return True
