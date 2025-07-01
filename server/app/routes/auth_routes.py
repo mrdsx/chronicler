9ff5347e3f94e91c2dd7516d606bbb1b8b52ac2b
mrdsx <andre0.castlegames@gmail.com>
2025-07-01 16:00:19 +0300

    refactor(backend/auth): move login validation logic to auth_services

--- server/app/routes/auth_routes.py
+++ server/app/routes/auth_routes.py
@@ -1,11 +1,8 @@
-from fastapi import APIRouter, HTTPException, Security
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
-from utils.validation_utils import get_is_login_data_valid
-from db.users_db import get_user_by_email
 from schemas.auth_schemas import AuthSchema_SignUp, AuthSchema_Login
-from services.auth_services import validate_signup_data
+from services.auth_services import validate_login_data, validate_signup_data
 from services.users_services import create_user
 from constants import routes
 from auth import Auth
@@ -29,14 +26,7 @@ async def signup(signup_data: AuthSchema_SignUp):
 
 @router.post("/login")
 async def login(user_details : AuthSchema_Login):
-    db_user = get_user_by_email(user_details.email)
-    if db_user is None:
-        raise HTTPException(status_code=401, detail="Wrong email or password")
-    
-    email = user_details.email
-    password = user_details.password
-    if not get_is_login_data_valid(email, password, db_user["hashed_password"]):
-        raise HTTPException(status_code=401, detail="Wrong email or password")
+    validate_login_data()
     
     access_token = auth_handler.encode_token(user_details.email)
     refresh_token = auth_handler.encode_refresh_token(user_details.email)
--- server/app/services/auth_services.py
+++ server/app/services/auth_services.py
@@ -1,10 +1,22 @@
 from fastapi import HTTPException
 
-from schemas.auth_schemas import AuthSchema_SignUp
+from schemas.auth_schemas import AuthSchema_Login, AuthSchema_SignUp
+from db.users_db import get_user_by_email
 from utils.auth_utils import validate_email_address, validate_password_length, validate_user_not_exists
+from utils.validation_utils import get_is_login_data_valid
 from auth import Auth
 
 auth_handler = Auth()
+
+def validate_login_data(login_data: AuthSchema_Login):
+    db_user = get_user_by_email(login_data.email)
+    if db_user is None:
+        raise HTTPException(status_code=401, detail="Wrong email or password")
+    
+    email = login_data.email
+    password = login_data.password
+    if not get_is_login_data_valid(email, password, db_user["hashed_password"]):
+        raise HTTPException(status_code=401, detail="Wrong email or password")
 
 # TODO: add password symbols validation (lower- and uppercase letters, numbers, special characters)
 def validate_signup_data(signup_data: AuthSchema_SignUp):
