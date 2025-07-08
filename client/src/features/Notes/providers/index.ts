203bbf688400aadb83265d3a26ef2ea48a29524d
mrdsx <andre0.castlegames@gmail.com>
2025-07-08 17:00:49 +0300

    refactor(frontend/notes): add index file inside notes providers folder

---
+++ client/src/features/Notes/providers/index.ts
@@ -0,0 +1,11 @@
+import { NoteEditorRefsContextProvider } from "./NoteEditorRefsContextProvider";
+import { NotesContextProvider } from "./NotesContextProvider";
+import { SearchNotesContextProvider } from "./SearchNotesContextProvider";
+import { SelectedNoteContextProvider } from "./SelectedNoteContextProvider";
+
+export {
+  NoteEditorRefsContextProvider,
+  NotesContextProvider,
+  SearchNotesContextProvider,
+  SelectedNoteContextProvider,
+};
