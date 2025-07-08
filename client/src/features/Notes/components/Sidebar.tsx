b26d70f72cd56bdefeee7c4f237d2bcae69f962f
mrdsx <andre0.castlegames@gmail.com>
2025-07-08 20:53:39 +0300

    chore(frontend): add navigation to home page to title in sidebar in notes page

--- client/src/features/Notes/components/Sidebar.tsx
+++ client/src/features/Notes/components/Sidebar.tsx
@@ -2,11 +2,14 @@ import { AddNoteBtn, SearchNotesBar } from "@/features/Notes/components";
 import { ToggleThemeBtn } from "../../../components/custom/ToggleThemeBtn";
 import { Button } from "../../../components/ui";
 import { CircleUserRound, Home, NotepadText } from "lucide-react";
+import { ROUTES } from "@/routes";
 
 export function Sidebar() {
   return (
     <div className="flex h-full w-[20%] flex-col gap-6 rounded-md border-1 border-(--border-color) bg-(--sidebar) p-4">
-      <span className="ml-1 text-2xl">Chronicler</span>
+      <a href={ROUTES.HOME}>
+        <span className="ml-1 text-2xl">Chronicler</span>
+      </a>
       <div className="flex items-center gap-2">
         <CircleUserRound />
         <h2 className="text-lg">Username</h2>
