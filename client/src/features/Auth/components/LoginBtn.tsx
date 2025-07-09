0b6244bdd0008f16bfb0e260d3b2d69aa180e09d
mrdsx <andre0.castlegames@gmail.com>
2025-07-09 22:16:20 +0300

    refactor(frontend/auth): replace navigation by useNavigate with Link tag in Login and SignUp buttons

--- client/src/features/Auth/components/LoginBtn.tsx
+++ client/src/features/Auth/components/LoginBtn.tsx
@@ -1,25 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ROUTES } from "@/routes";
 import { Button, buttonVariants } from "@/components/ui";
 import type { VariantProps } from "class-variance-authority";
 
 export function LoginBtn({
-  className,
   variant,
   size,
   ...props
 }: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>) {
-  const navigate = useNavigate();
-
   return (
-    <Button
-      className={className}
-      variant={variant}
-      size={size}
-      onClick={() => navigate(ROUTES.LOGIN)}
-      {...props}
-    >
-      Log In
-    </Button>
+    <Link to={ROUTES.LOGIN}>
+      <Button variant={variant} size={size} {...props}>
+        Log In
+      </Button>
+    </Link>
   );
 }
--- client/src/features/Auth/components/SignUpBtn.tsx
+++ client/src/features/Auth/components/SignUpBtn.tsx
@@ -1,25 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ROUTES } from "@/routes";
 import { Button, buttonVariants } from "@/components/ui";
 import type { VariantProps } from "class-variance-authority";
 
 export function SignUpBtn({
-  className,
   variant,
   size,
   ...props
 }: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>) {
-  const navigate = useNavigate();
-
   return (
-    <Button
-      className={className}
-      variant={variant}
-      size={size}
-      onClick={() => navigate(ROUTES.SIGNUP)}
-      {...props}
-    >
-      Sign up
-    </Button>
+    <Link to={ROUTES.SIGNUP}>
+      <Button variant={variant} size={size} {...props}>
+        Sign up
+      </Button>
+    </Link>
   );
 }
