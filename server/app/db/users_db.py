024aa663f48e2d1a5f82f043c6d40238383b99d7
mrdsx <andre0.castlegames@gmail.com>
2025-07-11 17:32:12 +0300

    refactor(backend/db): update return types in users_db.py

--- server/app/db/users_db.py
+++ server/app/db/users_db.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -12,21 +13,21 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(engine)
 session = Session()
 
-def save_user(user: DB_User_Model):
+def save_user(user: DB_User_Model) -> None:
     new_user = DB_User_Model(username=user["username"],
                              email=user["email"],
                              hashed_password=user["hashed_password"])
     session.add(new_user)
     session.commit()
 
-def get_user_by_username(username: str) -> UserSchema | None:
+def get_user_by_username(username: str) -> Optional[UserSchema]:
     try:
         db_user = session.query(DB_User_Model).filter(DB_User_Model.username == username).first()
         return UserSchema.model_validate(db_user).model_dump()
     except BaseException:
         return None
 
-def get_user_by_email(email: str) -> UserSchema | None:
+def get_user_by_email(email: str) -> Optional[UserSchema]:
     try:
         db_user = session.query(DB_User_Model).filter(DB_User_Model.email == email).first()
         return UserSchema.model_validate(db_user).model_dump()
