5ff79e9e93d11a37a1143be2ab5bdcffa07e0015
mrdsx <andre0.castlegames@gmail.com>
2025-07-11 18:24:51 +0300

    feat(frontend/auth): remove unecessary added input in email input component

--- client/src/features/Auth/components/AuthForm/EmailInput.tsx
+++ client/src/features/Auth/components/AuthForm/EmailInput.tsx
@@ -12,7 +12,6 @@ export function EmailInput({ register }: AuthFormInputProps<AuthFormInputInt>) {
         required
         {...register("email")}
       />
-      <input type="text" />
     </div>
   );
 }
