01a05da36b447a6492117032c2cd8d4457b60758
mrdsx <andre0.castlegames@gmail.com>
2025-07-12 14:17:09 +0300

    feat(frontend/auth): add register fetch function & update login fetch function types

--- client/src/features/Auth/api/Auth.ts
+++ client/src/features/Auth/api/Auth.ts
@@ -1,24 +1,40 @@
 import { apiClient } from "../../../api/client";
-import type { LoginFormInputInt } from "@/features/Auth/types";
+import type {
+  LoginFormInputInt,
+  SignUpFormInputInt,
+} from "@/features/Auth/types";
 import type { AccessTokensResponse } from "./types";
 import { isErrorResponse } from "@/api";
 
-type ReturnType = AccessTokensResponse;
-
 export async function loginUser(
   loginFormData: LoginFormInputInt,
-): Promise<ReturnType> {
+): Promise<AccessTokensResponse> {
   const res = await apiClient("/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(loginFormData),
   });
-  const data: ReturnType = await res.json();
+  const data: AccessTokensResponse = await res.json();
 
   if (isErrorResponse(data)) throw new Error(data.detail);
 
   if (!res.ok)
     throw new Error("An unexpected error occurred while logging in.");
+
+  return data;
+}
+
+export async function registerUser(signUpFormData: SignUpFormInputInt) {
+  const res = await apiClient("/auth/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(signUpFormData),
+  });
+  const data = await res.json();
+
+  if (isErrorResponse(data)) throw new Error(data.detail);
+
+  if (!res.ok) throw new Error("An unexpected error occurred while signing up");
 
   return data;
 }
