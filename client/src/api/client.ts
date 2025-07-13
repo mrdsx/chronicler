b25a918fc06d615e279a9ce0866076d8ea2dae80
mrdsx <andre0.castlegames@gmail.com>
2025-07-13 17:04:27 +0300

    feat(frontend/api): add handling errors in api client

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,5 +1,18 @@
+import { isErrorResponse } from "./utils";
+
 const API_BASE_URL = "http://127.0.0.1:3000/api";
 
-export async function apiClient(path: string, options?: RequestInit) {
-  return await fetch(`${API_BASE_URL}${path}`, options);
+export async function apiClient(
+  path: string,
+  options?: RequestInit,
+  errorMsg?: string,
+) {
+  const res = await fetch(`${API_BASE_URL}${path}`, options);
+  const data = await res.json();
+
+  if (isErrorResponse(data)) throw new Error(data.detail);
+
+  if (!res.ok) throw new Error(errorMsg || "Unexpected error");
+
+  return data;
 }
