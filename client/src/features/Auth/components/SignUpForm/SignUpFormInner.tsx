ed9f437f2318026fc06302dabb7c860ab6c9f54b
mrdsx <andre0.castlegames@gmail.com>
2025-07-13 17:30:53 +0300

    refactor(frontend/auth): replace manual fetching with useMutation in signup form

--- client/src/features/Auth/components/SignUpForm/SignUpFormInner.tsx
+++ client/src/features/Auth/components/SignUpForm/SignUpFormInner.tsx
@@ -12,39 +12,32 @@ import {
   ConfirmPasswordInput,
 } from "../AuthForm";
 import { FormFooter } from "./FormFooter";
-import { useState } from "react";
 import { LoaderCircle } from "lucide-react";
+import { registerUser, type AccessTokensResponse } from "../../api";
+import { useMutation } from "@tanstack/react-query";
 
 export function SignUpFormInner() {
   const navigate = useNavigate();
   const { register, handleSubmit: submitHandler } = useForm<AuthFormInputInt>();
 
-  const [isDisabled, setIsDisabled] = useState(false);
-
-  // TODO: refactor
-  async function handleSubmit(signUpData: SignUpFormInputInt): Promise<void> {
-    try {
-      setIsDisabled(true);
-      const res = await fetch("http://127.0.0.1:3000/api/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(signUpData),
-      });
-      const data = await res.json();
-
-      if (!res.ok) {
-        toast.error(data.detail);
-        return;
-      }
-
+  const { mutate, isPending } = useMutation<
+    AccessTokensResponse,
+    Error,
+    SignUpFormInputInt
+  >({
+    mutationFn: registerUser,
+    onSuccess: (data) => {
       localStorage.setItem("access_token", data.access_token);
       navigate(ROUTES.MAIN);
-    } catch (error) {
-      console.error("Sign up failed:", error);
-      toast.error("Failed to create an account. Please try again.");
-    } finally {
-      setIsDisabled(false);
-    }
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error(error.message);
+    },
+  });
+
+  function handleSubmit(loginFormData: SignUpFormInputInt) {
+    mutate(loginFormData);
   }
 
   return (
@@ -57,10 +50,10 @@ export function SignUpFormInner() {
         <ConfirmPasswordInput register={register} />
         <Button
           className="min-w-full"
-          disabled={isDisabled}
-          aria-busy={isDisabled}
+          disabled={isPending}
+          aria-busy={isPending}
         >
-          {isDisabled ? (
+          {isPending ? (
             <LoaderCircle className="animate-spin" />
           ) : (
             "Create an account"
