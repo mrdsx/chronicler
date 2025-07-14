9bf3d5b1b92c72e50e5993b3d9d72253930ccd0f
mrdsx <andre0.castlegames@gmail.com>
2025-07-14 16:30:42 +0300

    feat(frontend/notes): remove fetching user info in notes page

--- client/src/pages/Notes.tsx
+++ client/src/pages/Notes.tsx
@@ -1,31 +1,7 @@
-import { useEffect } from "react";
 import { NotesArea, Sidebar } from "@/features/Notes/components";
 import { NotesProvider } from "@/features/Notes/providers/NotesProvider";
 
 export function Notes() {
-  const accessToken = localStorage.getItem("access_token");
-
-  useEffect(() => {
-    async function getUser() {
-      const res = await fetch("http://127.0.0.1:3000/api/users", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      const data = await res.json();
-
-      if (!res.ok) {
-        console.log(data.detail);
-        return;
-      }
-
-      console.log(data.username);
-    }
-
-    getUser();
-  }, []);
-
   return (
     <NotesProvider>
       <div className="box-border flex h-[100vh] gap-2 bg-(--background) p-2">
