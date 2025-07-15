07fb428a399e250985ab2f4b39c5253c1ffaa169
mrdsx <andre0.castlegames@gmail.com>
2025-07-15 16:37:53 +0300

    refactor(frontend/api): add generic typing in apiClient

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -2,11 +2,11 @@ import { isErrorResponse } from "./utils";
 
 const API_BASE_URL = "http://127.0.0.1:3000/api";
 
-export async function apiClient(
+export async function apiClient<TResponse extends Record<string, any>>(
   path: string,
   options?: RequestInit,
   errorMsg?: string,
-) {
+): Promise<TResponse> {
   const res = await fetch(`${API_BASE_URL}${path}`, options);
   const data = await res.json();
 
