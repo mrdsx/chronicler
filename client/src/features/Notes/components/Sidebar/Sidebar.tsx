b58a02436db68bd5995d7974d90be5aa5d8f9125
mrdsx <andre0.castlegames@gmail.com>
2025-07-15 16:26:43 +0300

    feat(frontend/user): add fetching user info in sidebar

--- client/src/features/Notes/components/Sidebar/Sidebar.tsx
+++ client/src/features/Notes/components/Sidebar/Sidebar.tsx
@@ -3,12 +3,27 @@ import { UserInfo } from "./UserInfo";
 import { NoteActions } from "./NoteActions";
 import { Navigation } from "./Navigation";
 import { ToggleThemeBtn } from "../../../Theme/components/ToggleThemeBtn";
+import { useEffect, useState } from "react";
+import { useUserInfo } from "@/features/User/useUserInfo";
+import { SkeletonLoader } from "./SkeletonLoader";
 
 export function Sidebar() {
+  const { getUserInfo } = useUserInfo();
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchUserInfo() {
+      const userInfo = await getUserInfo();
+      setUsername(userInfo?.username);
+    }
+
+    fetchUserInfo();
+  }, []);
+
   return (
     <aside className="flex h-full w-[20%] flex-col gap-6 rounded-md border-1 border-(--border-color) bg-(--sidebar) p-4">
       <Logo className="ml-1" />
-      <UserInfo />
+      {username ? <UserInfo username={username} /> : <SkeletonLoader />}
       <NoteActions />
       <Navigation />
       <ToggleThemeBtn />
