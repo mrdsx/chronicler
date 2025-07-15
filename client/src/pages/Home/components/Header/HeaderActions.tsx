9c2747b4ed63677aefda9baa53438e0d47e26afa
mrdsx <andre0.castlegames@gmail.com>
2025-07-15 19:54:39 +0300

    feat(frontend/user): add account button in case user is logged in

--- client/src/pages/Home/components/Header/HeaderActions.tsx
+++ client/src/pages/Home/components/Header/HeaderActions.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui";
 import { LoginBtn, SignUpBtn } from "@/features/Auth/components";
 import type { User } from "@/features/User/types";
 
@@ -5,7 +6,7 @@ export function HeaderActions({ user }: { user: User | undefined }) {
   return (
     <div className="flex gap-2">
       {user ? (
-        `Hello, ${user.username}`
+        <Button variant="outline">Account</Button>
       ) : (
         <>
           <LoginBtn variant="outline" />
