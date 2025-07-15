851f6d36049dd0c9cb80ce4145ed53291feb7b39
mrdsx <andre0.castlegames@gmail.com>
2025-07-15 18:20:20 +0300

    chore(frontend/api): add api base url import in apiClient

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,6 +1,5 @@
+import { API_BASE_URL } from "./constants";
 import { isErrorResponse } from "./utils";
-
-const API_BASE_URL = "http://127.0.0.1:3000/api";
 
 export async function apiClient<TResponse extends Record<string, any>>(
   path: string,
