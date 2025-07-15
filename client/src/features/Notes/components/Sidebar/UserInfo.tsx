81be4f83c835c2e2e4cdca5e5911b5148a171106
mrdsx <andre0.castlegames@gmail.com>
2025-07-15 17:42:50 +0300

    refactor(frontend/user): move fetching user in UserInfo component

--- client/src/features/Notes/components/Sidebar/UserInfo.tsx
+++ client/src/features/Notes/components/Sidebar/UserInfo.tsx
@@ -1,10 +1,34 @@
 import { CircleUserRound } from "lucide-react";
+import { ROUTES } from "@/routes";
+import { SkeletonLoader } from "./SkeletonLoader";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useUserInfo } from "@/features/User/useUserInfo";
 
-export function UserInfo({ username }: { username?: string }) {
+export function UserInfo() {
+  const { getUserInfo } = useUserInfo();
+  const navigate = useNavigate();
+
+  const { data, isError, isPending } = useQuery({
+    queryKey: ["user"],
+    queryFn: getUserInfo,
+  });
+
+  useEffect(() => {
+    if (isError) {
+      navigate(ROUTES.LOGIN);
+    }
+  }, [isError]);
+
+  if (isPending || !data) {
+    return <SkeletonLoader />;
+  }
+
   return (
     <div className="flex items-center gap-2">
       <CircleUserRound />
-      <h2 className="text-lg">{username}</h2>
+      <h2 className="text-lg">{data.username}</h2>
     </div>
   );
 }
