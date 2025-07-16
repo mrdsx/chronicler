a584f6359058441cc2102c3315523ab944ebc0c9
mrdsx <andre0.castlegames@gmail.com>
2025-07-16 15:15:36 +0300

    chore(frontend): add ui component 'dropdown-menu' in index exports

--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export { Button, buttonVariants } from "./button";
 export { Card, CardContent } from "./card";
+export * from "./dropdown-menu";
 export { Input } from "./input";
 export { Label } from "./label";
 export { Toaster } from "./sonner";
