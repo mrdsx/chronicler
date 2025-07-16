c8d8389f05dc77d2a57cc684f45756ed278d2413
mrdsx <andre0.castlegames@gmail.com>
2025-07-16 15:52:10 +0300

    refactor(frontend/user): extract account dropdown menu into component

---
+++ client/src/pages/Home/components/Header/AccountDropdownMenu.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+export function AccountDropdownMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button>Account</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- client/src/pages/Home/components/Header/HeaderActions.tsx
+++ client/src/pages/Home/components/Header/HeaderActions.tsx
@@ -1,29 +1,12 @@
-import { Button } from "@/components/ui";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-} from "@/components/ui/dropdown-menu";
 import { LoginBtn, SignUpBtn } from "@/features/Auth/components";
 import type { User } from "@/features/User/types";
+import { AccountDropdownMenu } from "./AccountDropdownMenu";
 
 export function HeaderActions({ user }: { user: User | undefined }) {
   return (
     <div className="flex gap-2">
       {user ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button>Account</Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <AccountDropdownMenu />
       ) : (
         <>
           <LoginBtn variant="outline" />
