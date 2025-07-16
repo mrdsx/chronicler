5cdc0936692ce7873b30ef44df94e412279d2e62
mrdsx <andre0.castlegames@gmail.com>
2025-07-16 15:47:13 +0300

    chore(frontend): remove ui component 'dropdown-menu' from index file exports

--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -1,6 +1,5 @@
 export { Button, buttonVariants } from "./button";
 export { Card, CardContent } from "./card";
-export * from "./dropdown-menu";
 export { Input } from "./input";
 export { Label } from "./label";
 export { Toaster } from "./sonner";
