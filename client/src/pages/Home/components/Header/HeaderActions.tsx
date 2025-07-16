0895009300c1afcf99fc8ade7df83f2abacba1f5
mrdsx <andre0.castlegames@gmail.com>
2025-07-16 15:43:22 +0300

    feat(frontend/user): add dropdown menu in header on home page

--- client/src/pages/Home/components/Header/HeaderActions.tsx
+++ client/src/pages/Home/components/Header/HeaderActions.tsx
@@ -1,4 +1,11 @@
 import { Button } from "@/components/ui";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 import { LoginBtn, SignUpBtn } from "@/features/Auth/components";
 import type { User } from "@/features/User/types";
 
@@ -6,7 +13,17 @@ export function HeaderActions({ user }: { user: User | undefined }) {
   return (
     <div className="flex gap-2">
       {user ? (
-        <Button>Account</Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button>Account</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <>
           <LoginBtn variant="outline" />
