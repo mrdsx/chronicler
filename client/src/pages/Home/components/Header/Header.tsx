68a096f30b6f21be858e77300e3a3d38716043a4
mrdsx <andre0.castlegames@gmail.com>
2025-07-17 17:44:39 +0300

    feat(frontend): add bottom border to home page header

--- client/src/pages/Home/components/Header/Header.tsx
+++ client/src/pages/Home/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Logo } from "@/components/Logo";
 
 export function Header() {
   return (
-    <header>
+    <header className="border-border border-b-1">
       <nav className="mx-auto flex justify-between bg-(--home-background) px-4 py-2.5 lg:px-6">
         <Logo className="flex font-semibold text-indigo-500" />
         <AccountActions />
