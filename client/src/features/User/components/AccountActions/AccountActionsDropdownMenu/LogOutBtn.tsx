839698ee2ba53b7f1438fe10e202e08bb54659bf
mrdsx <andre0.castlegames@gmail.com>
2025-07-17 16:54:49 +0300

    feat(frontend/user): add actions when clicking logout button

--- client/src/features/User/components/AccountActions/AccountActionsDropdownMenu/LogOutBtn.tsx
+++ client/src/features/User/components/AccountActions/AccountActionsDropdownMenu/LogOutBtn.tsx
@@ -1,9 +1,15 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { deleteUserAccessToken } from "@/features/User/utils/userAccessTokenUtils";
 import { LogOut } from "lucide-react";
 
 export function LogOutBtn() {
+  function handleClick(): void {
+    deleteUserAccessToken();
+    window.location.reload();
+  }
+
   return (
-    <DropdownMenuItem variant="destructive">
+    <DropdownMenuItem variant="destructive" onClick={handleClick}>
       <LogOut />
       Logout
     </DropdownMenuItem>
