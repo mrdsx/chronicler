c520b65bee4a78b2b9940a296d811d8eaaa06c45
mrdsx <andre0.castlegames@gmail.com>
2025-07-17 16:04:08 +0300

    chore(frontend/user): edit UserInfo styles in Notes page Sidebar

--- client/src/features/Notes/components/Sidebar/Sidebar.tsx
+++ client/src/features/Notes/components/Sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ export function Sidebar() {
   return (
     <aside className="flex h-full w-[20%] flex-col gap-6 rounded-md border-1 border-(--border-color) bg-(--sidebar) p-4">
       <Logo className="ml-1" />
-      <UserInfo />
+      <UserInfo className="text-lg" />
       <NoteActions />
       <Navigation />
       <ToggleThemeBtn />
