b6356113ccfd6f162567107c7a5f740861b9792a
mrdsx <andre0.castlegames@gmail.com>
2025-07-17 16:08:11 +0300

    feat(frontend/user): add navigation to notes page in notes button

--- client/src/features/User/components/AccountActions/AccountActionsDropdownMenu/NotesBtn.tsx
+++ client/src/features/User/components/AccountActions/AccountActionsDropdownMenu/NotesBtn.tsx
@@ -1,9 +1,17 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { ROUTES } from "@/routes";
 import { NotepadText } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export function NotesBtn() {
+  const navigate = useNavigate();
+
+  function handleClick(): void {
+    navigate(ROUTES.MAIN);
+  }
+
   return (
-    <DropdownMenuItem>
+    <DropdownMenuItem onClick={handleClick}>
       <NotepadText />
       Notes
     </DropdownMenuItem>
