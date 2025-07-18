bac2d911807cf16aad88b3e7ac0c5a64b43f2655
mrdsx <andre0.castlegames@gmail.com>
2025-07-18 15:48:46 +0300

    refactor(frontend/auth): add auth forms mutation hooks

---
+++ client/src/features/Auth/hooks/useAuthMutations.ts
@@ -0,0 +1,51 @@
+import { useMutation } from "@tanstack/react-query";
+import { loginUser, registerUser, type AccessTokensResponse } from "../api";
+import type { LoginFormInputInt, SignUpFormInputInt } from "../types";
+import { setUserAccessToken } from "@/features/User/utils/userAccessTokenUtils";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "@/routes";
+import { toast } from "sonner";
+
+export function useSignUpFormMutation() {
+  const navigate = useNavigate();
+
+  const { mutate, isPending } = useMutation<
+    AccessTokensResponse,
+    Error,
+    SignUpFormInputInt
+  >({
+    mutationFn: registerUser,
+    onSuccess: (data) => {
+      setUserAccessToken(data.access_token);
+      navigate(ROUTES.MAIN);
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error(error.message);
+    },
+  });
+
+  return { mutate, isPending };
+}
+
+export function useLoginFormMutation() {
+  const navigate = useNavigate();
+
+  const { mutate, isPending } = useMutation<
+    AccessTokensResponse,
+    Error,
+    LoginFormInputInt
+  >({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      setUserAccessToken(data.access_token);
+      navigate(ROUTES.MAIN);
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error(error.message);
+    },
+  });
+
+  return { mutate, isPending };
+}
