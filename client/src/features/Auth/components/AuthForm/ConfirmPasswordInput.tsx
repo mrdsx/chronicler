c372fe8d753e73b8230a56c16ce27ea2a7826e9d
mrdsx <andre0.castlegames@gmail.com>
2025-07-18 15:02:43 +0300

    feat(frontend/auth): add registerOptions prop in ConfirmPasswordInput

--- client/src/features/Auth/components/AuthForm/ConfirmPasswordInput.tsx
+++ client/src/features/Auth/components/AuthForm/ConfirmPasswordInput.tsx
@@ -1,8 +1,11 @@
 import { Input, Label } from "@/components/ui";
 import type { AuthFormInputProps, SignUpFormInputInt } from "../../types";
 
+// TODO: extract error msg and Input styles
 export function ConfirmPasswordInput({
   register,
+  error,
+  registerOptions,
 }: AuthFormInputProps<SignUpFormInputInt>) {
   return (
     <div className="grid gap-3">
@@ -11,8 +14,9 @@ export function ConfirmPasswordInput({
         id="confirm-password"
         type="password"
         required
-        {...register("confirm_password")}
+        {...register("confirm_password", registerOptions)}
       />
+      {error && <p className="text-sm text-red-500">{error.message}</p>}
     </div>
   );
 }
