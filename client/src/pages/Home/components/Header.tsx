9a013dcdbe55e01f2bb749cf0c91dad793784337
mrdsx <andre0.castlegames@gmail.com>
2025-07-20 12:17:52 +0300

    feat(frontend): add logo image in home page header

--- client/src/pages/Home/components/Header.tsx
+++ client/src/pages/Home/components/Header.tsx
@@ -1,3 +1,4 @@
+import { LogoImage } from "@/components/LogoImage";
 import { AccountActions } from "../../../features/User/components/AccountActions/AccountActions";
 import { Logo } from "@/components/Logo";
 
@@ -5,7 +6,10 @@ export function Header() {
   return (
     <header className="border-border border-b-1">
       <nav className="mx-auto flex justify-between bg-(--home-background) px-4 py-2.5 lg:px-6">
-        <Logo className="flex font-semibold text-indigo-500" />
+        <div className="flex items-center gap-2">
+          <LogoImage width={"1.5rem"} />
+          <Logo className="flex font-semibold text-indigo-600" />
+        </div>
         <AccountActions />
       </nav>
     </header>
