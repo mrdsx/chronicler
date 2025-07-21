187488c12b690c4bcf8df7cf90cf92c83f2c3236
mrdsx <andre0.castlegames@gmail.com>
2025-07-21 17:42:32 +0300

    refactor(backend/auth): break validate_username util function into smaller functions

--- server/app/utils/auth_utils/username.py
+++ server/app/utils/auth_utils/username.py
@@ -4,6 +4,11 @@ from validators import get_is_username_valid
 
 
 def validate_username(username: str) -> None:
+    validate_username_length(username)
+    validate_username_pattern(username)
+
+
+def validate_username_length(username: str) -> None:
     if len(username) < 3:
         raise HTTPException(
             status_code=422, detail="Username must be at least 3 characters long"
@@ -12,6 +17,9 @@ def validate_username(username: str) -> None:
         raise HTTPException(
             status_code=422, detail="Username must be at most 20 characters long"
         )
+
+
+def validate_username_pattern(username: str) -> None:
     if not get_is_username_valid(username):
         raise HTTPException(
             status_code=422,
