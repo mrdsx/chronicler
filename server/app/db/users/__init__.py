dbe357081f936a9c32222e20eae5b43d81bde071
mrdsx <andre0.castlegames@gmail.com>
2025-07-24 15:34:28 +0300

    refactor(backend/db): update users package imports in __init__.py

--- server/app/db/users/__init__.py
+++ server/app/db/users/__init__.py
@@ -1,3 +1,3 @@
-from .users_db import save_user
+from .users_db import save_user, get_user_by_username, get_user_by_email
 
-__all__ = ["save_user"]
+__all__ = ["save_user", "get_user_by_username", "get_user_by_email"]
