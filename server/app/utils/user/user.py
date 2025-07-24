49c3500b3dffc35673ae34a923f3efb66ac8eea6
mrdsx <andre0.castlegames@gmail.com>
2025-07-24 16:44:02 +0300

    refactor(backend/users): add explicit check whether user found or not

--- server/app/utils/user/user.py
+++ server/app/utils/user/user.py
@@ -4,12 +4,12 @@ from db.users import get_user_by_email, get_user_by_username
 
 
 def validate_user_not_exists(username: str, email: str) -> None:
-    if get_user_by_username(username):
+    if get_user_by_username(username) is not None:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="Username is already taken"
         )
 
-    if get_user_by_email(email):
+    if get_user_by_email(email) is not None:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="Email is already associated with account",
