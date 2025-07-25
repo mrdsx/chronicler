2ab3f04ef753949d7ecd5876f0fc6b6d53bc95fa
mrdsx <andre0.castlegames@gmail.com>
2025-07-25 15:41:45 +0300

    refactor(backend/users): edit error response in get_user_info endpoint

--- server/app/routes/users_routes.py
+++ server/app/routes/users_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Security
+from fastapi import APIRouter, HTTPException, Security, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jwt import PyJWTError
 
@@ -6,7 +6,7 @@ import endpoints
 from auth import Auth
 from db.users import get_user_by_email
 from schemas.users_schemas import PublicUserSchema
-from utils.auth import get_email_from_auth_credentials, raise_exception_invalid_token
+from utils.auth import get_email_from_auth_credentials
 
 auth_handler = Auth()
 security = HTTPBearer()
@@ -19,4 +19,7 @@ def get_user_info(credentials: HTTPAuthorizationCredentials = Security(security)
         email = get_email_from_auth_credentials(credentials)
         return get_user_by_email(email)
     except PyJWTError:
-        raise_exception_invalid_token()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Failed to retrieve user data",
+        )
