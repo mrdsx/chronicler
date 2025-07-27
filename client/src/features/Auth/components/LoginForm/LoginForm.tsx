87208abb0b4026c06d1a8f1ea3f397264005d780
mrdsx <andre0.castlegames@gmail.com>
2025-07-27 21:07:36 +0300

    refactor(frontend/auth): add components composition on login page

--- client/src/features/Auth/components/LoginForm/LoginForm.tsx
+++ client/src/features/Auth/components/LoginForm/LoginForm.tsx
@@ -1,16 +1,3 @@
-import { Card, CardContent } from "../../../../components/ui";
-import { FormBackground } from "@/features/Auth/components/AuthForm/FormBackground";
-import { LoginFormInner } from "./LoginFormInner";
-
-export function LoginForm() {
-  return (
-    <div className="grid gap-6">
-      <Card className="overflow-hidden p-0">
-        <CardContent className="grid p-0 md:grid-cols-2">
-          <LoginFormInner />
-          <FormBackground />
-        </CardContent>
-      </Card>
-    </div>
-  );
+export function LoginForm({ children }: { children: React.ReactNode }) {
+  return <div className="grid gap-6">{children}</div>;
 }
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,19 @@
+import { Card, CardContent } from "@/components/ui";
 import { LoginForm } from "@/features/Auth/components";
+import { FormBackground } from "@/features/Auth/components/AuthForm/FormBackground";
+import { LoginFormInner } from "@/features/Auth/components/LoginForm/LoginFormInner";
 
 export function Login() {
   return (
     <div className="flex h-screen items-center justify-center bg-(--home-background)">
-      <LoginForm />
+      <LoginForm>
+        <Card className="overflow-hidden p-0">
+          <CardContent className="grid p-0 md:grid-cols-2">
+            <LoginFormInner />
+            <FormBackground />
+          </CardContent>
+        </Card>
+      </LoginForm>
     </div>
   );
 }
