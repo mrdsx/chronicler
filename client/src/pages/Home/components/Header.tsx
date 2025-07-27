285965b49328fea1e05cb898dc84ec6b00e1d57a
mrdsx <andre0.castlegames@gmail.com>
2025-07-27 21:04:14 +0300

    feat(frontend): update styles in Header component

--- client/src/pages/Home/components/Header.tsx
+++ client/src/pages/Home/components/Header.tsx
@@ -1,8 +1,7 @@
 export function Header({ children }: { children: React.ReactNode }) {
-  // TODO: add items-center in <nav />
   return (
     <header className="border-border border-b-1">
-      <div className="mx-auto flex justify-between bg-(--home-background) px-4 py-2.5 lg:px-6">
+      <div className="mx-auto flex items-center justify-between bg-(--home-background) px-4 py-2.5 lg:px-6">
         {children}
       </div>
     </header>
