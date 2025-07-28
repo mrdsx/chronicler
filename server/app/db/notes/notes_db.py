66e42015ac9c4a0921b02f6fadc3325816b20d57
mrdsx <andre0.castlegames@gmail.com>
2025-07-28 15:53:22 +0300

    refactor(backend/notes): add missing type annotations in notes_db.py

--- server/app/db/notes/notes_db.py
+++ server/app/db/notes/notes_db.py
@@ -12,13 +12,13 @@ Session = sessionmaker(engine)
 session = Session()
 
 
-def delete_note_by_id(note_id: int):
+def delete_note_by_id(note_id: int) -> DB_Note_Model | None:
     db_note = session.query(DB_Note_Model).filter(DB_Note_Model.id == note_id).first()
     session.delete(db_note)
     session.commit()
 
 
-def get_note_by_id(note_id: int):
+def get_note_by_id(note_id: int) -> DB_Note_Model | None:
     try:
         db_note = (
             session.query(DB_Note_Model).filter(DB_Note_Model.id == note_id).first()
