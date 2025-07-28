9462b2fd5147f0cb96943236c4d9a43ff0921702
mrdsx <andre0.castlegames@gmail.com>
2025-07-28 18:15:14 +0300

    chore(frontend): edit imports order in index.tsx

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
 import "./styles/index.css";
 
 const queryClient = new QueryClient();
