93bf98239e3f82ad57de8ae12634e87ec082d113
mrdsx <andre0.castlegames@gmail.com>
2025-07-28 18:34:16 +0300

    chore(frontend/notes): remove comment in NotesList component

--- client/src/features/Notes/components/NotesList/NotesList.tsx
+++ client/src/features/Notes/components/NotesList/NotesList.tsx
@@ -30,7 +30,6 @@ export function NotesList() {
 
   if (isError) toast.error("Failed to fetch notes");
 
-  // TODO: remove border, add divide-x in NotesArea component
   return (
     <div className="w-[30%] border-r-1 border-r-(--border-color)">
       <h2 className="pt-3 pb-2 pl-4 text-xl">Notes</h2>
