671e21121d6a393363982e311514b45ef511651b
mrdsx <andre0.castlegames@gmail.com>
2025-07-29 10:27:01 +0300

    refactor(frontend/auth): update auth components index file

--- client/src/features/Auth/components/index.ts
+++ client/src/features/Auth/components/index.ts
@@ -1,3 +1,6 @@
+export { FormBackground } from "./AuthForm/FormBackground";
+export { FormInner as LoginFormInner } from "./LoginForm/FormInner";
+export { FormInner as SignUpFormInner } from "./SignUpForm/FormInner";
 export { LoginBtn } from "./LoginBtn";
 export { LoginForm } from "./LoginForm/LoginForm";
 export { SignUpBtn } from "./SignUpBtn";
