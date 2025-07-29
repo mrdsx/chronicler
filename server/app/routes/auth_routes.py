a888333a5bcffabcf6e0712699577c75d2f0220e
mrdsx <andre0.castlegames@gmail.com>
2025-07-29 16:07:44 +0300

    feat(backend/auth): add missing async/await keywords in sign_up endpoint

--- server/app/routes/auth_routes.py
+++ server/app/routes/auth_routes.py
@@ -11,8 +11,8 @@ router = APIRouter(prefix=endpoints.AUTH)
 
 
 @router.post("/register", response_model=AuthSchema_Tokens)
-def sign_up(signup_data: AuthSchema_SignUp):
-    validate_signup_data(signup_data)
+async def sign_up(signup_data: AuthSchema_SignUp):
+    await validate_signup_data(signup_data)
     user = create_user(signup_data)
 
     access_token = auth_handler.encode_token(user["email"])
