4452e5010c687972aabbf95f52c8e0aa205a4ba2
mrdsx <andre0.castlegames@gmail.com>
2025-07-30 17:36:17 +0300

    feat(frontend/api): change check for error in apiClient

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -10,14 +10,12 @@ export async function apiClient<TResponse extends Record<string, any>>(
   const res = await fetch(`${API_BASE_URL}${path}`, options);
   const data = await res.json();
 
+  if (res.ok) return data;
   if (isErrorResponse(data)) throw new Error(data.detail);
-  if (!res.ok) {
-    const { error, message } = data.detail;
-    if (error !== undefined && message !== undefined) {
-      throw new APIError(error, message);
-    }
-    throw new Error(errorMsg || "An error occurred while fetching data");
-  }
 
-  return data;
+  const { error, message } = data.detail;
+  if (error !== undefined && message !== undefined) {
+    throw new APIError(error, message);
+  }
+  throw new Error(errorMsg || "An error occurred while fetching data");
 }
