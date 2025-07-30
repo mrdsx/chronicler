5e1e8e5af7d8604e65c04f8786ed82cf40d5a4b9
mrdsx <andre0.castlegames@gmail.com>
2025-07-30 17:10:04 +0300

    feat(frontend/notes): implement saving cloned note in CloneNoteBtn on click event

--- client/src/features/Notes/components/NoteItem/NoteItemDropdownMenu/CloneNoteBtn.tsx
+++ client/src/features/Notes/components/NoteItem/NoteItemDropdownMenu/CloneNoteBtn.tsx
@@ -1,17 +1,37 @@
+import { QUERY_KEYS } from "@/api";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import type { APINote } from "@/features/notes/api";
-import { useSelectedNoteContext } from "@/features/notes/hooks/context";
+import {
+  createNote,
+  type APINote,
+  type CreateNoteInput,
+} from "@/features/notes/api";
+import {
+  useNotesContext,
+  useSelectedNoteContext,
+} from "@/features/notes/hooks/context";
 import { useNotes } from "@/features/notes/hooks/useNotes";
+import { useMutation } from "@tanstack/react-query";
 import { Copy } from "lucide-react";
+import { toast } from "sonner";
 
 export function CloneNoteBtn({ targetNote }: { targetNote: APINote }) {
+  const { notes, setNotes } = useNotesContext();
   const { setSelectedNote } = useSelectedNoteContext();
-  const { handleCreateNote, selectNoteTitleInput } = useNotes();
+  const { selectNoteTitleInput } = useNotes();
+
+  const { mutate } = useMutation<APINote | null, Error, CreateNoteInput>({
+    mutationKey: [QUERY_KEYS.CREATE_NOTE],
+    mutationFn: createNote,
+    onSuccess: (note) => {
+      setNotes([...notes, note as APINote]);
+      setSelectedNote(note);
+    },
+    onError: (error) => toast.error(error.message),
+  });
 
   function handleClick(): void {
     const newNoteTitle = targetNote.title + " - Clone";
-    const newNote = handleCreateNote(newNoteTitle, targetNote.content);
-    setTimeout(() => setSelectedNote(newNote), 1);
+    mutate({ title: newNoteTitle, content: targetNote.content });
     setTimeout(selectNoteTitleInput, 220);
   }
 
