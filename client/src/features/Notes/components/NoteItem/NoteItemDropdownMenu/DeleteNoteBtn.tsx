ffdafbd5cd34c14f3f44395e0ea041678a5aeb9b
mrdsx <andre0.castlegames@gmail.com>
2025-07-31 15:47:59 +0300

    refactor(frontend/notes): replace useMutation with useDeleteNoteMutation in DeleteNoteBtn

--- client/src/features/Notes/components/NoteItem/NoteItemDropdownMenu/DeleteNoteBtn.tsx
+++ client/src/features/Notes/components/NoteItem/NoteItemDropdownMenu/DeleteNoteBtn.tsx
@@ -1,32 +1,10 @@
-import { QUERY_KEYS, type BaseAPIResponse } from "@/api";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import { deleteNote, type APINote } from "@/features/notes/api";
-import {
-  useNotesContext,
-  useSelectedNoteContext,
-} from "@/features/notes/hooks";
-import { useMutation } from "@tanstack/react-query";
+import { type APINote } from "@/features/notes/api";
+import { useDeleteNoteMutation } from "@/features/notes/hooks/useNotesMutations";
 import { Trash2 } from "lucide-react";
-import { toast } from "sonner";
 
 export function DeleteNoteBtn({ targetNote }: { targetNote: APINote }) {
-  const { notes, setNotes } = useNotesContext();
-  const { selectedNote, setSelectedNote } = useSelectedNoteContext();
-
-  const { mutate } = useMutation<BaseAPIResponse, Error, number>({
-    mutationKey: [QUERY_KEYS.DELETE_NOTE],
-    mutationFn: deleteNote,
-    onSuccess: () => {
-      const newNotes = notes.filter((note) => note.id !== targetNote.id);
-      setNotes([...newNotes]);
-      if (selectedNote?.id === targetNote.id) {
-        setTimeout(() => setSelectedNote(null), 1);
-      } else {
-        setTimeout(() => setSelectedNote(selectedNote), 1);
-      }
-    },
-    onError: (error) => toast.error(error.message),
-  });
+  const { mutate } = useDeleteNoteMutation(targetNote);
 
   function handleClick(): void {
     mutate(targetNote.id);
